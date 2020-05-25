566f57f2e4f9097f721434e6f6d8c891c8d46636
ryanhudson <ryanhudson@ryans-mbp.lan>
2020-05-25 15:27:24 -0400

    add and remove course now work

--- src/Gradebook.java
+++ src/Gradebook.java
@@ -3,13 +3,16 @@ import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumn;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Ryan Hudson
  */
-public class Gradebook extends JFrame {
+public class Gradebook extends JFrame implements ActionListener {
 
 	private JTextArea textArea;
+	private static JTextField identifierInput;
 	protected Container contentPane;
 	private static DefaultTableModel cdtm;
 	private static DefaultTableModel gdtm;
@@ -56,19 +59,38 @@ public class Gradebook extends JFrame {
         // ------------------------Creating the panel for the buttons------------------------
         JPanel buttons = new JPanel();
         buttons.setVisible(true);
+        
+        // Initialize buttons and add action listeners
         JButton addCourse = new JButton("Add Course");
+        addCourse.addActionListener(this);
+        
         JButton removeCourse = new JButton("Remove Course");
+        removeCourse.addActionListener(this);
+        
         JButton editCourse = new JButton("Edit Course");
+        editCourse.addActionListener(this);
+        
         JButton enterGrade = new JButton("Enter Grade");
+        enterGrade.addActionListener(this);
+        
         JButton finalizeGrades = new JButton("Finalize Grades");
+        finalizeGrades.addActionListener(this);
+        
         JButton saveChanges = new JButton("Save Changes");
+        saveChanges.addActionListener(this);
+        
         JButton manualOverride = new JButton("Manual Override");
+        manualOverride.addActionListener(this);
+        
+        // Create the textfield for allowing edits
         JLabel identifier = new JLabel("Enter Identifier: ");
-        JTextField identifierInput = new JTextField();
+        identifierInput = new JTextField();
         identifierInput.setPreferredSize(new Dimension(150,20));
         identifierInput.setEditable(true);
         identifierInput.setBounds(10,10,300,50);
         identifierInput.setSize(200, 20);
+        
+        // Add the buttons to the panel
         buttons.add(addCourse);
         buttons.add(removeCourse);
         buttons.add(editCourse);
@@ -78,6 +100,8 @@ public class Gradebook extends JFrame {
         buttons.add(saveChanges);
         buttons.add(identifier);
         buttons.add(identifierInput);
+        
+        // Add the panel to the frame
         frame.add(buttons, BorderLayout.CENTER);
         
         // ------------------------Creating the table for the list of grades/assignments------------------------
@@ -101,12 +125,65 @@ public class Gradebook extends JFrame {
 
 	}
 	
-	public void addCourse(String title, String prof, String time, String identifier, String credits, String numGrade, String gMode, String fGrade, String year) {
+	public void addCourse() {
+		
+		String title = JOptionPane.showInputDialog("Enter Course Title");
+		
+		String prof = JOptionPane.showInputDialog("Enter Professor Name");
+		
+		String time = JOptionPane.showInputDialog("Enter Course Day/Time");
+		
+		String identifier = JOptionPane.showInputDialog("Enter Course Identifier");
+		
+		String credits = JOptionPane.showInputDialog("Enter Number of Credits");
+		
+		String numGrade = "n/a";
+		
+		String[] gModeChoices = {"C", "P/NP"};
+		String gMode = (String) JOptionPane.showInputDialog(null, "Select Grade Mode", "Course Master", JOptionPane.QUESTION_MESSAGE, null, gModeChoices, gModeChoices[0]);
+		
+		String fGrade = "n/a";
+		
+		String[] yearChoices = {"1", "2", "3", "4", "5"};
+		String year = (String) JOptionPane.showInputDialog(null, "Select Year", "Course Master", JOptionPane.QUESTION_MESSAGE, null, yearChoices, yearChoices[0]);
+
 		cdtm.addRow(new Object[] {title, prof, time, identifier, credits, numGrade, gMode, fGrade, year});
+	}
+	
+	public void removeCourse() {
+		for(int i = 0; i < cdtm.getRowCount(); i++) {
+			if(cdtm.getValueAt(i, 3).equals(identifierInput.getText())) {
+				String courseName = (String) cdtm.getValueAt(i, 0);
+				if(JOptionPane.showConfirmDialog(null, "Are you sure you want to delete " + courseName + "? This operation cannot be undone.") == 0) {
+					removeAssociatedGrades();
+					cdtm.removeRow(i);
+				}
+				else return;
+			}
+		}
+	}
+	
+	// removes grades associated with a deleted course
+	public void removeAssociatedGrades() {
+		return;
 	}
 
 	public static void main(String[] a) {
 		new Gradebook();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String s = e.getActionCommand();
+		
+		if(s.equalsIgnoreCase("Add Course")) {
+			addCourse();
+		}
+		
+		if(s.equalsIgnoreCase("Remove Course")) {
+			removeCourse();
+		}
+		
 	}
 
 }
